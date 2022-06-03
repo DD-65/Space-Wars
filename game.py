422e9c85785a9607048c5caf7139e8b1f8fa3201
Daniel <96732834+DD-65@users.noreply.github.com>
2022-06-03 21:38:34 +0200

    Update game.py

--- game.py
+++ game.py
@@ -31,18 +31,18 @@ class planet:
 
 #BEISPIEL:                     ENTFERNUNG   BEREISBAR T/F
 #BEISPIEL:   NAME  PLANETENSYSTEM    WARPTOR T/F    BILD
-sonne=planet("Sonne","Milchstraße",0,False,False,tkinter.PhotoImage(file="./fotos/ship.gif"))
-erde=planet("Erde","Milchstraße",250,False,True,tkinter.PhotoImage(file="./fotos/erde.gif"))
-mars=planet("Mars","Milchstraße",370,False,True,tkinter.PhotoImage(file="./fotos/mars.gif"))
-venus=planet("Venus-Raumstation","Milchstraße",100,False,True,tkinter.PhotoImage(file="./fotos/venus.gif"))
-uranus=planet("Uranus-Raumstation","Milchstraße",600,False,True,tkinter.PhotoImage(file="./fotos/uranus.gif"))
-pluto=planet("Pluto","Milchstraße",1100,False,True,tkinter.PhotoImage(file="./fotos/pluto.gif"))
-warptorS=planet("Warptor","Milchstraße",1000,True,True,tkinter.PhotoImage(file="./fotos/warp.gif"))
+sonne=planet("Sonne","Milchstraße",0,False,False,tkinter.PhotoImage(file="./assets/fotos/ship.gif"))
+erde=planet("Erde","Milchstraße",250,False,True,tkinter.PhotoImage(file="./assets/fotos/erde.gif"))
+mars=planet("Mars","Milchstraße",370,False,True,tkinter.PhotoImage(file="./assets/fotos/mars.gif"))
+venus=planet("Venus-Raumstation","Milchstraße",100,False,True,tkinter.PhotoImage(file="./assets/fotos/venus.gif"))
+uranus=planet("Uranus-Raumstation","Milchstraße",600,False,True,tkinter.PhotoImage(file="./assets/fotos/uranus.gif"))
+pluto=planet("Pluto","Milchstraße",1100,False,True,tkinter.PhotoImage(file="./assets/fotos/pluto.gif"))
+warptorS=planet("Warptor","Milchstraße",1000,True,True,tkinter.PhotoImage(file="./assets/fotos/warp.gif"))
 # PLANETEN VON ALPHA CENTAURI
-alphacentauria=planet("Alpha Centauri A (Stern)","Alpha Centauri",0,False,False,tkinter.PhotoImage(file="./fotos/ship.gif"))
-alphacentauriab=planet("Alpha Centauri Ab","Alpha Centauri",150,False,True,tkinter.PhotoImage(file="./fotos/ship.gif"))
-alphacentauribb=planet("Alpha Centauri Bb","Alpha Centauri",2580,False,True,tkinter.PhotoImage(file="./fotos/ship.gif"))
-warptorA=planet("Warptor","Alpha Centauri",1000,True,True,tkinter.PhotoImage(file="./fotos/warp.gif"))
+alphacentauria=planet("Alpha Centauri A (Stern)","Alpha Centauri",0,False,False,tkinter.PhotoImage(file="./assets/fotos/ship.gif"))
+alphacentauriab=planet("Alpha Centauri Ab","Alpha Centauri",150,False,True,tkinter.PhotoImage(file="./assets/fotos/ship.gif"))
+alphacentauribb=planet("Alpha Centauri Bb","Alpha Centauri",2580,False,True,tkinter.PhotoImage(file="./assets/fotos/ship.gif"))
+warptorA=planet("Warptor","Alpha Centauri",1000,True,True,tkinter.PhotoImage(file="./assets/fotos/warp.gif"))
 
 sPlaneten= [sonne,erde,mars,venus,uranus,pluto,warptorS] # PLANETEN DES SONNENSYSTEMS
 aPlaneten=[alphacentauria,alphacentauriab,alphacentauribb,warptorA] # PLANETEN UM ALPHA CENTAURI
@@ -174,7 +174,7 @@ def NeuesSpiel():
     Laderaum = 100
     treibstoff=100
     global locationImg
-    locationImg=tkinter.PhotoImage(file="./fotos/ship.gif")
+    locationImg=tkinter.PhotoImage(file="./assets/fotos/ship.gif")
     EingabeMenge.delete("0","end")
     EingabeMenge.insert("end","0")
     sternsystem=sonnensystem
@@ -766,7 +766,7 @@ TreibstoffZustand=tkinter.Label(game,text=treibstofftext)
 TreibstoffZustand.grid(row=8,column=2,padx=5, columnspan=2)
 # ---------------------
 global locationImg
-locationImg=tkinter.PhotoImage(file="./fotos/ship.gif")
+locationImg=tkinter.PhotoImage(file="./assets/fotos/ship.gif")
 global fotolabel
 fotolabel=tkinter.Label(image=locationImg)
 fotolabel.grid(row=1,column=0,rowspan=8)
